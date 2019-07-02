955fee504ee65b586018b0feda305d73cd91d2c0
Evgeny Nabokov <evgenyn@pressreader.com>
2019-07-02 16:45:55 -0700

    Built dist.

--- dist/bloodhound.js
+++ dist/bloodhound.js
@@ -1,5 +1,5 @@
 /*!
- * typeahead.js 1.2.1
+ * typeahead.js 1.3.1
  * https://github.com/corejavascript/typeahead.js
  * Copyright 2013-2019 Twitter, Inc. and other contributors; Licensed MIT
  */
@@ -174,7 +174,7 @@
             }
         };
     }();
-    var VERSION = "1.2.1";
+    var VERSION = "1.3.1";
     var tokenizers = function() {
         "use strict";
         return {
